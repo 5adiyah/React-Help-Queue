0d6b20db5e2e7ddd4d8f45cd9d084c539d3c2c50
5adiyah <sadiali@pdx.edu>
2018-06-04 22:10:47 -0700

    Config babel

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,24 @@ module.exports = {
 
   resolve: {
     extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "es2015",
+            "react"
+          ]
+        }
+      },
+    ],
   }
+
+  // ^ Hey Webpack, use this babel-loader tool to transpile our JSX into ES5 in this React project. You can find the JSX in files with a .jsx extension. But ignore .jsx files in the nodemodules directory.”
 
 };
